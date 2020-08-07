c732814508f6d60ccc68a807c191c36bc1f1dc28
Sven Schittecatte <s.schittecatte@ccvlab.eu>
2020-08-07 22:13:28 +0200

    Setting up dev-setup script

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,3 +1,22 @@
-sudo apt update
-sudo apt install gtop
-echo "yeah"
+#!/bin/bash
+
+set -x
+
+cd ~
+git clone git@github.com:PsycheClone/dev-setup.git
+cd dev-setup
+
+./installp.sh COMMON
+./installp.sh ZSH
+./installp.sh PYTHON
+./installp.sh NEOVIM
+./installp.sh ALACRITTY
+./installp.sh NVM
+
+cd ~
+mkdir .cfg
+git clone --bare git@github.com:PsycheClone/dotfiles.git $HOME/.cfg
+alias c='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+c checkout
+c config --local status.showUntrackedFiles no
+
