d97c024c2a481404e05811421518722327d493f1
Sven Schittecatte <s.schittecatte@ccvlab.eu>
2020-08-08 21:29:30 +0200

    Updating

--- installp.sh
+++ installp.sh
@@ -5,6 +5,10 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NOCOLOR='\033[0m'
 
+if [ ! -d "$XDG_CACHE_HOME/dev-setup" ]; then
+    mkdir $XDG_CACHE_HOME/dev-setup
+fi
+
 if [ ! -f "$XDG_CACHE_HOME/dev-setup/$1" ]; then
     sh "$HOME/dev-setup/packages/$1".sh && \
         touch $XDG_CACHE_HOME/dev-setup/$1 && \
